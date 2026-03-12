7cb65ae0cd07ed1dacbde3d4a9a985a2fa6df175
Hareesh-Reddy-9126 <hareeshreddy916@gmail.com>
2026-03-12 16:43:21 +0530

    Add skill-12 project

---
+++ skill-12/backend/src/main/java/com/example/studentapp/StudentAppApplication.java
@@ -0,0 +1,11 @@
+package main.java.com.example.studentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentAppApplication.class, args);
+    }
+}
